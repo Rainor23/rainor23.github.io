335e90cfea01089f051ad9d9740fa7dd79b18853
Rainer-Michael Kennedy <45594693+Rainor23@users.noreply.github.com>
2020-12-02 17:53:11 +0000

    Add files via upload

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "USERS ARE DANGEROUS",
+        "excerpt":"Users are at the forefront of most experiences when external users or customers interact with a company. Ranging from customer services to IT technicians, users will be using either published applications or those created in house for tasks specific to the company. With users having access to systems, comes the...","categories": ["Cybersecurity"],
+        "tags": ["users","dangerous"],
+        "url": "http://0.0.0.0:4000/cybersecurity/Users-are-dangerous/",
+        "teaser": null
+      },{
+        "title": "SUPER REVERSE ENGINEERING",
+        "excerpt":" def print():   print(\"testing\")  if i in range(6)  ","categories": ["reverse_engineering"],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/reverse_engineering/Super-Crazy-Reversing/",
+        "teaser": null
+      },{
+        "title": "JUICESHOP LEVEL 1",
+        "excerpt":"Here we will go through Juiceshop Level 1 Curabitur blandit tempus porttitor. Nullam quis risus eget urna mollis ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit. Level 1 Challenge Description Bonus Payload Use the bonus payload &lt;iframe width=\"100%\" height=\"166\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/771984076&amp;color=%23ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true\"&gt;&lt;/iframe&gt; in...","categories": ["Juiceshop","OWASP"],
+        "tags": ["Juiceshop","OWASP"],
+        "url": "http://0.0.0.0:4000/juiceshop/owasp/JuiceBox-Level-1-Walkthrough/",
+        "teaser": null
+      }]
